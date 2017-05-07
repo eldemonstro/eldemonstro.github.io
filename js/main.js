ced2171245e4a32172668f9ce07fc74b85fa5f18
Fabricio Bezerra <demonstro003@gmail.com>
2017-05-06 22:21:03 -0300

    Calcula a media e fiz o readme

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var numeroClasses = 3;
 var amplitude = 10;
 var menorNumero = 0;
-var tBody = $('.table').find('tbody');
+var tBody = $('.table').find('#tbody');
+var valoresChave = {};
 
 $(() => {
     inicializar();
@@ -45,7 +46,7 @@ function montarLinha() {
 }
 
 function limparTabela() {
-    $.each($('tbody>tr'), function (index) {
+    $.each($('#tbody>tr'), function (index) {
         var newIndex = amplitude * (index);
         $(this).remove();
     });
@@ -58,7 +59,7 @@ function atualizarTabela() {
     var ximul2fitotal = [];
     var frtotal = [];
 
-    $.each($('tbody>tr'), function (index) {
+    $.each($('#tbody>tr'), function (index) {
         var newIndex = (amplitude * index) + parseFloat(menorNumero);
         var nextIndex = parseFloat(amplitude) + parseFloat(newIndex);
         var pMedio = (newIndex + nextIndex) / 2;
@@ -81,23 +82,103 @@ function atualizarTabela() {
     var fitotalsum = somaArray(fitotal);
     var ximulfisum = somaArray(ximulfitotal);
     var ximul2fisum = somaArray(ximul2fitotal);
-    console.log(somaArray(ximul2fitotal));
 
     $('#fitotal').text(fitotalsum);
     $('#ximul2fitotal').text(ximul2fisum);
     $('#ximulfitotaltotal').text(ximulfisum);
 
-    $.each($('tbody>tr'), function (index) {
+    $.each($('#tbody>tr'), function (index) {
         var currentFi = $(this).find('#fiInput').val();
         var fr = (currentFi / fitotalsum) * 100;
         $(this).find('#fr').text(fr.toFixed(4));
         frtotal.push(fr);
     });
+
     var frtotalsum = somaArray(frtotal);
     if (frtotalsum > 99.9) {
         frtotalsum = 100;
     }
+
     $('#frtotal').text(frtotalsum.toFixed(0));
+
+    valoresChave.sumfi = fitotalsum;
+    valoresChave.sumxifi = ximulfisum;
+    valoresChave.sumxi2fi = ximul2fisum;
+    valoresChave.sumfr = frtotalsum;
+
+    var q3class = (3 * fitotalsum) / 3;
+    var q1class = (1 * fitotalsum) / 3;
+    var p90class = (90 * fitotalsum) / 100;
+    var p10class = (10 * fitotalsum) / 100;
+
+    var classeModal = {};
+    var classeQ3 = {};
+    var classeQ1 = {};
+    var classeP10 = {};
+    var classeP90 = {};
+
+    var modaRecorde = -1;
+
+    $.each($('#tbody>tr'), function (index) {
+        var newIndex = (amplitude * index) + parseFloat(menorNumero);
+        var nextIndex = parseFloat(amplitude) + parseFloat(newIndex);
+        if ($(this).find('#fiInput').val() > modaRecorde) {
+            classeModal = montarObjetoModal($(this), newIndex);
+            modaRecorde = $(this).find('#fiInput').val();
+        }
+        if ($(this).find('#fac').text() < q3class) {
+            classeQ3 = montarClasseQP($(this), newIndex);
+        }
+        if ($(this).find('#fac').text() < q1class) {
+            classeQ1 = montarClasseQP($(this), newIndex);
+        }
+        if ($(this).find('#fac').text() < p90class) {
+            classeP10 = montarClasseQP($(this), newIndex);
+        }
+        if ($(this).find('#fac').text() < p10class) {
+            classeP90 = montarClasseQP($(this), newIndex);
+        }
+    });
+
+    valoresChave.classeModal = classeModal;
+    classeQ3.fracmin = q3class;
+    classeQ1.fracmin = q1class;
+    classeP10.fracmin = p10class;
+    classeP90.fracmin = p90class;
+    valoresChave.classeQ3 = classeQ3;
+    valoresChave.classeQ1 = classeQ1;
+    valoresChave.classeP10 = classeP10;
+    valoresChave.classeP90 = classeP90;
+    atualizarResultados();
+}
+
+function atualizarResultados() {
+    console.log(valoresChave);
+    var media = valoresChave.sumxifi / valoresChave.sumfi;
+    var contas = $('.contas');
+    console.log(contas);
+    contas.find('#xifi').text(valoresChave.sumxifi);
+    contas.find('#fi').text(valoresChave.sumfi);
+    contas.find('#mediaResultado').text(media);
+}
+
+function montarClasseQP(linha, index) {
+    var qp = {};
+    qp.lq = index;
+    qp.fi = Math.floor($(this).find('#fiInput').val());
+    qp.facant = Math.floor(linha.next('tr').find('#fac').text());
+    return qp;
+}
+
+function montarObjetoModal(linha, index) {
+    var modal = {};
+    modal.lmo = index;
+    modal.fi = Math.floor(linha.find('#fiInput').val());
+    var finext = Math.floor(linha.next('tr').find('#fiInput').val());
+    modal.finext = finext || 0;
+    var fiprev = Math.floor(linha.prev('tr').find('#fiInput').val());
+    modal.fiprev = fiprev || 0;
+    return modal;
 }
 
 $('#classesInput').on('input', function (event) {
@@ -120,7 +201,6 @@ $('#menorvalorInput').on('input', function (event) {
 
 function fiInput(event) {
     atualizarTabela();
-    console.log(event);
 }
 
 function somaArray(arr) {
