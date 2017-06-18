8350a21ce0bd0bb2b5765dd6075f74c07cda796a
Fabricio Bezerra <demonstro003@gmail.com>
2017-06-18 13:23:45 -0300

    IHC quase pronto

--- IHC/js/index.js
+++ IHC/js/index.js
@@ -1,6 +1,44 @@
 $(document).ready(function() {
-  $('.bxslider').bxSlider({
+  $('.viagem-slider').bxSlider({
     adaptiveHeight: true,
-    mode: 'fade'
+    mode: 'fade',
+    controls: false
   });
+
+  $('.relatos-slider').bxSlider({
+    adaptiveHeight: true,
+    mode: 'fade',
+    auto: true,
+    autoControls: false,
+    pause: 4000,
+    controls: false
+  });
+
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr('tabindex', '-1');
+              $target.focus();
+            };
+          });
+        }
+      }
+    });
 });
